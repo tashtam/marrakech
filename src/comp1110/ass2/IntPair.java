4eb26121bed7c96b6b4d8b72d1281ad91509daa6
bridge <wxlxy316@163.com>
2023-08-19 16:23:40 +1000

    revert the unexpected change on IntPair.java

--- src/comp1110/ass2/IntPair.java
+++ src/comp1110/ass2/IntPair.java
@@ -2,19 +2,6 @@ package comp1110.ass2;
 
 // Tashia
 public class IntPair {
-    private final int x;
-    private final int y;
-
-    IntPair(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
+    int x;
+    int y;
 }
