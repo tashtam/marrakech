1a109b9386e78f171a9933d0cf4ff3317742afe3
tashiatamara <67252129+tashiatamara@users.noreply.github.com>
2023-08-19 23:32:31 +1000

    Added code documentation for IntPair.java and Player.java.

--- src/comp1110/ass2/IntPair.java
+++ src/comp1110/ass2/IntPair.java
@@ -1,6 +1,10 @@
 package comp1110.ass2;
 
 // Tashia
+
+/**
+ * The class IntPair defines a pair of integers used to determine positions on the board.
+ */
 public class IntPair {
     int x;
     int y;
--- src/comp1110/ass2/Player.java
+++ src/comp1110/ass2/Player.java
@@ -1,12 +1,28 @@
 package comp1110.ass2;
 
 // Tashia
+/**
+ * The class Player defines the players currently participating in the game.
+ * Each player 4 fields: color, coins, remainingRugNumber, and out.
+ * Parameter color: each player's color.
+ * Parameter coins: the number of coins each player has.
+ * Parameter remainingRugNumber: the remaining number of rugs each player has.
+ * Parameter out: whether each player is in or out of the game.
+ *
+ * Each player has the method payTo.
+ */
 public class Player {
     char color;
     int coins;
     int remainingRugNumber;
     boolean out;
 
+
+    /**
+     * This method is for paying coins/dirhams to another player when the current player steps on another player's rug.
+     * @param other the player receiving payment from the current player
+     * @param coins the amount of coins to be paid
+     */
     void payTo(Player other, int coins){};
 }
 
