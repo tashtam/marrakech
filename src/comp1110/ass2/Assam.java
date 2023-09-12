0df085a8cc14fcaffb43a0ec9405ee54c44afe97
Xinyang Li <u7760022@anu.edu.au>
2023-09-13 08:26:38 +1000

    fix assam constructor error

--- src/comp1110/ass2/Assam.java
+++ src/comp1110/ass2/Assam.java
@@ -1,6 +1,7 @@
 package comp1110.ass2;
 
 // jiangbei
+
 /**
  * this class defines Assam that players need to control,there are
  * two fields named: (direction, position) and two methods: (rotate, move)
@@ -9,28 +10,46 @@ package comp1110.ass2;
  */
 public class Assam {
 
-    char direction;
+    int degree;
     IntPair position;
 
-    public Assam(String assamString){
-        this.direction = assamString.charAt(1);
+    public int getDegree() {
+        return degree;
+    }
+
+    public IntPair getPosition() {
+        return position;
+    }
+
+    public Assam(String assamString) {
+        char d = assamString.charAt(3);
+        if (d == 'N') this.degree = 0;
+        else if (d == 'E') this.degree = 90;
+        else if (d == 'S') this.degree = 180;
+        else this.degree = 270;
         this.position = new IntPair(
-                assamString.charAt(2) - '0',
-                assamString.charAt(3) - '0'
+                assamString.charAt(1) - '0',
+                assamString.charAt(2) - '0'
         );
     }
 
-    /**rotate method will rotate Assam clockwise of 90 degree or anticlockwise of 90 degree
+    /**
+     * rotate method will rotate Assam clockwise of 90 degree or anticlockwise of 90 degree
      * or not rotate
+     *
      * @param degree an int number that can be 90(rotate clockwise for 90 degree)
      *               -90(rotate anticlockwise for 90 degree)
      *               0 not rotate
      */
-    void rotate(int degree) {};
+    void rotate(int degree) {
+    }
 
-    /**this method will move Assam forward at present direction for steps of dieResult
+    /**
+     * this method will move Assam forward at present direction for steps of dieResult
+     *
      * @param dieResult a random int number stands for die result from a set{1,2,2,3,3,4}
      *                  each element of the set has the same probability
      */
-    void move(int dieResult) {};
+    void move(int dieResult) {
+    }
 }
