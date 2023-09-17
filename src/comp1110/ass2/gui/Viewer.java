642439db71d74553d0221b19a40fdf9a652f8c15
Xinyang Li <u7760022@anu.edu.au>
2023-09-17 18:15:23 +1000

    fix: viewer update twice will lead to overlap

--- src/comp1110/ass2/gui/Viewer.java
+++ src/comp1110/ass2/gui/Viewer.java
@@ -26,6 +26,124 @@ public class Viewer extends Application {
     private final Group controls = new Group();
     private TextField boardTextField;
 
+    // constant
+    private Font font = Font.font("Consolas", FontWeight.EXTRA_BOLD, 16);
+
+    void displayBoard(Marrakech game) {
+        for (Tile tile : game.getBoard().getTiles()) {
+            Rectangle r1 = new Rectangle(game.TILE_SIZE, game.TILE_SIZE);
+            Rug rug = tile.getRug();
+            char color = ' ';
+            if (rug != null) color = rug.getColor();
+            r1.setFill(game.getJavaFxColor(color));
+            r1.setStrokeWidth(2);
+            r1.setStroke(Color.BLACK);
+            r1.setArcWidth(10);
+            r1.setArcHeight(10);
+
+            Text text = new Text();
+            text.setFont(this.font);
+
+            if (rug != null) text.setText(String.format("%02d", rug.getId()));
+
+            int layoutX = game.OFFSET_X + (game.TILE_SIZE + game.TILE_GAP) * tile.getPosition().getX();
+            r1.setLayoutX(layoutX);
+            int layoutY = game.OFFSET_Y + (game.TILE_SIZE + game.TILE_GAP) * tile.getPosition().getY();
+            r1.setLayoutY(layoutY);
+            text.setLayoutX(layoutX + game.TILE_SIZE / 2 + 10);
+            text.setLayoutY(layoutY + game.TILE_SIZE / 2 + 20);
+            this.root.getChildren().add(r1);
+            this.root.getChildren().add(text);
+        }
+    }
+
+    void displayAssam(Marrakech game) {
+        Assam assam = game.getAssam();
+        IntPair pos = assam.getPosition();
+        var players = game.getPlayers();
+        var index = game.getCurrentPlayerIndex();
+        var player = players[index];
+
+        Rectangle r1 = new Rectangle(game.TILE_SIZE / 2 + 5, game.TILE_SIZE / 2 + 5);
+        r1.setFill(game.getJavaFxColor(player.getColor()));
+        r1.setStrokeWidth(2);
+        r1.setStroke(Color.BLACK);
+        r1.setArcWidth(10);
+        r1.setArcHeight(10);
+
+        Text text = new Text();
+        text.setFont(font);
+        text.setText("" + assam.getDegree());
+
+        StackPane stack = new StackPane(r1, text);
+        stack.setLayoutX(game.OFFSET_X + (game.TILE_SIZE + game.TILE_GAP) * pos.getX());
+        stack.setLayoutY(game.OFFSET_Y + (game.TILE_SIZE + game.TILE_GAP) * pos.getY());
+        this.root.getChildren().add(stack);
+    }
+
+    void displayInfo(Marrakech game) {
+        int x = game.OFFSET_X + (game.TILE_SIZE + game.TILE_GAP) * game.getBoard().HEIGHT + 90;
+        int y = game.OFFSET_Y;
+        var rect = new Rectangle(x, y, 400, 200);
+        rect.setFill(Color.web("#eee"));
+        this.root.getChildren().add(rect);
+
+        this.displayCurrentPlayerInfo(game);
+        this.displayPlayersInfo(game);
+    }
+
+    void displayCurrentPlayerInfo(Marrakech game) {
+        var player = game.getPlayers()[game.getCurrentPlayerIndex()];
+        var color = player.getColor();
+        var javaFxColor = game.getJavaFxColor(color);
+
+        var text = new Text();
+        text.setFont(font);
+        text.setFill(javaFxColor);
+        text.setText("current player: " + color);
+
+        int x = game.OFFSET_X + (game.TILE_SIZE + game.TILE_GAP) * game.getBoard().HEIGHT + 100;
+        int y = game.OFFSET_Y + 20;
+        text.setLayoutX(x);
+        text.setLayoutY(y);
+        this.root.getChildren().add(text);
+    }
+
+    void displayPlayersInfo(Marrakech game) {
+        int x = game.OFFSET_X + (game.TILE_SIZE + game.TILE_GAP) * game.getBoard().HEIGHT + 100;
+        int y = game.OFFSET_Y + 60;
+
+        var text = new Text();
+        text.setFont(font);
+        text.setText("color / dirhams / rugs / out");
+        text.setLayoutX(x);
+        text.setLayoutY(y);
+        this.root.getChildren().add(text);
+
+        var players = game.getPlayers();
+        for (int i = 0; i < players.length; i++) {
+            var player = players[i];
+            var color = player.getColor();
+            var javaFxColor = game.getJavaFxColor(color);
+            var content = String.format(
+                    "%c / %02d / %02d / %b",
+                    player.getColor(),
+                    player.getCoins(),
+                    player.getRemainingRugNumber(),
+                    player.isOut()
+            );
+
+            text = new Text();
+            text.setFont(font);
+            text.setFill(javaFxColor);
+            text.setText(content);
+
+            y = game.OFFSET_Y + i * 30 + 90;
+            text.setLayoutX(x);
+            text.setLayoutY(y);
+            this.root.getChildren().add(text);
+        }
+    }
 
     /**
      * Draw a placement in the window, removing any previously drawn placements
@@ -34,87 +152,9 @@ public class Viewer extends Application {
      */
     void displayState(String state) {
         Marrakech game = new Marrakech(state);
-        for (int i = 0; i < game.BOARD_WIDTH; i++) {
-            for (int j = 0; j < game.BOARD_HEIGHT; j++) {
-                Tile tile = game.getTile(new IntPair(i, j));
-                Rectangle r1 = new Rectangle(game.TILE_SIZE, game.TILE_SIZE);
-                Rug rug = tile.getRug();
-                char color = ' ';
-                if (rug != null) color = rug.getColor();
-                r1.setFill(game.getJavaFxColor(color));
-                r1.setStrokeWidth(2);
-                r1.setStroke(Color.BLACK);
-                r1.setArcWidth(10);
-                r1.setArcHeight(10);
-
-                Text text = new Text();
-                Font font = Font.font("Verdana", FontWeight.EXTRA_BOLD, 16);
-                text.setFont(font);
-
-                if (rug != null) text.setText(String.format("%02d", rug.getId()));
-
-//                StackPane stack = new StackPane(r1, text);
-                int layoutX = game.OFFSET_X + (game.TILE_SIZE + game.TILE_GAP) * j;
-                r1.setLayoutX(layoutX);
-                int layoutY = game.OFFSET_Y + (game.TILE_SIZE + game.TILE_GAP) * i;
-                r1.setLayoutY(layoutY);
-                text.setLayoutX(layoutX + game.TILE_SIZE / 2 + 10);
-                text.setLayoutY(layoutY + game.TILE_SIZE / 2 + 20);
-                this.root.getChildren().add(r1);
-                this.root.getChildren().add(text);
-            }
-        }
-
-        {
-            Assam assam = game.getAssam();
-            IntPair pos = assam.getPosition();
-            var players = game.getPlayers();
-            var index = game.getCurrentPlayerIndex();
-            var player = players[index];
-
-            Rectangle r1 = new Rectangle(game.TILE_SIZE / 2 + 5, game.TILE_SIZE / 2 + 5);
-            r1.setFill(game.getJavaFxColor(player.getColor()));
-            r1.setStrokeWidth(2);
-            r1.setStroke(Color.BLACK);
-            r1.setArcWidth(10);
-            r1.setArcHeight(10);
-
-            Text text = new Text();
-            Font font = Font.font("Verdana", FontWeight.EXTRA_BOLD, 16);
-            text.setFont(font);
-            text.setText("" + assam.getDegree());
-
-            StackPane stack = new StackPane(r1, text);
-            stack.setLayoutX(game.OFFSET_X + (game.TILE_SIZE + game.TILE_GAP) * pos.getY());
-            stack.setLayoutY(game.OFFSET_Y + (game.TILE_SIZE + game.TILE_GAP) * pos.getX());
-            this.root.getChildren().add(stack);
-        }
-
-        {
-            var players = game.getPlayers();
-            var index = game.getCurrentPlayerIndex();
-            var player = players[index];
-            Text text = new Text();
-            Font font = Font.font("Consolas", FontWeight.EXTRA_BOLD, 16);
-            text.setFont(font);
-            text.setFill(game.getJavaFxColor(player.getColor()));
-            text.setText("current player: " + player.getColor());
-            int infoLayoutX = (game.TILE_SIZE + game.TILE_GAP) * game.BOARD_WIDTH;
-            text.setLayoutX(game.OFFSET_X + infoLayoutX + 100);
-            text.setLayoutY(game.OFFSET_Y + 20);
-            this.root.getChildren().add(text);
-
-            for (int i = 0; i < players.length; i++) {
-                player = players[i];
-                text = new Text();
-                text.setFont(font);
-                text.setText(String.format("player: %c|dirhams: %02d|rugs: %02d", player.getColor(), player.getCoins(), player.getRemainingRugNumber()));
-                text.setFill(game.getJavaFxColor(player.getColor()));
-                text.setLayoutX(game.OFFSET_X + infoLayoutX + 100);
-                text.setLayoutY(game.OFFSET_Y + i * 30 + 60);
-                this.root.getChildren().add(text);
-            }
-        }
+        this.displayBoard(game);
+        this.displayAssam(game);
+        this.displayInfo(game);
         // FIXME Task 5: implement the simple state viewer [DONE]
     }
 
