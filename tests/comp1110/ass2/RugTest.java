813dfea905ee5156286b20082af5dcd3b3ed8b99
bridge <wxlxy316@163.com>
2023-10-01 21:00:27 +1100

    :memo: docs: update contribution and originality and code-review

--- tests/comp1110/ass2/RugTest.java
+++ tests/comp1110/ass2/RugTest.java
@@ -9,42 +9,40 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RugTest {
     @Test
-    void testRug(){
-        for (int i = 0; i < 200; i ++) {
+    void testRug() {
+        for (int i = 0; i < 200; i++) {
             //Added loop to run the test 200 times to decrease the chances of edge cases/issues happening
-        var rand = new Random();
+            var rand = new Random();
 
-        var colors = "cypr";
-        var id1 = rand.nextInt(10); // Number of bound of random (0-9)
-        var id2 = rand.nextInt(10); // Number of bound of random (0-9)
-        var positions0x = rand.nextInt(7); // Number of bound of random (0-6)
-        var positions0y = rand.nextInt(7);
-        var positions1x = rand.nextInt(7);
-        var positions1y = rand.nextInt(7);
+            var colors = "cypr";
+            var id1 = rand.nextInt(10); // Number of bound of random (0-9)
+            var id2 = rand.nextInt(10); // Number of bound of random (0-9)
+            var positions0x = rand.nextInt(7); // Number of bound of random (0-6)
+            var positions0y = rand.nextInt(7);
+            var positions1x = rand.nextInt(7);
+            var positions1y = rand.nextInt(7);
 
-        var color = colors.charAt( rand.nextInt(4));
-        // Number of bound of random (0-3)
-        // Each index number points to different letters in the String colors ("cypr")
+            var color = colors.charAt(rand.nextInt(4));
+            // Number of bound of random (0-3)
+            // Each index number points to different letters in the String colors ("cypr")
 
-        var rugString = "" + color + id1 + id2 + positions0x + positions0y + positions1x + positions1y;
+            var rugString = "" + color + id1 + id2 + positions0x + positions0y + positions1x + positions1y;
 
-        //char (like color) + int (id1, id2, etc.) = int rugString [Implicit typecasting]
-        //However, Rug() needs String as its parameter
-        //Therefore, we need to add "" before color in var rugString
-        //String (like "") + char + int = String rugString (because String is the strongest)
+            //char (like color) + int (id1, id2, etc.) = int rugString [Implicit typecasting]
+            //However, Rug() needs String as its parameter
+            //Therefore, we need to add "" before color in var rugString
+            //String (like "") + char + int = String rugString (because String is the strongest)
 
-        System.out.println(rugString);
+            System.out.println(rugString);
 
-        var rug = new Rug(rugString);
-        Assertions.assertEquals(rug.color, color);
-        Assertions.assertEquals(rug.id,  id1*10 + id2);
-        Assertions.assertEquals(rug.positions[0].x, positions0x);
-        Assertions.assertEquals(rug.positions[0].y, positions0y);
-        Assertions.assertEquals(rug.positions[1].x, positions1x);
-        Assertions.assertEquals(rug.positions[1].y, positions1y);
+            var rug = new Rug(rugString);
+            Assertions.assertEquals(rug.color, color);
+            Assertions.assertEquals(rug.id, id1 * 10 + id2);
+            Assertions.assertEquals(rug.positions[0].x, positions0x);
+            Assertions.assertEquals(rug.positions[0].y, positions0y);
+            Assertions.assertEquals(rug.positions[1].x, positions1x);
+            Assertions.assertEquals(rug.positions[1].y, positions1y);
+        }
     }
-
-}
-
 }
 
