538666008d44f8218e04a146ac368427d9c6009e
Administrator <u7759982@anu.edu.au>
2023-10-19 19:59:30 +1100

    :poop: test: Junit test for board

--- tests/comp1110/ass2/BoardTest.java
+++ tests/comp1110/ass2/BoardTest.java
@@ -4,23 +4,54 @@ import org.junit.jupiter.api.Test;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Objects;
 import java.util.Random;
 import java.util.stream.Stream;
-
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import static org.junit.jupiter.api.Assertions.*;
 class BoardTest {
+    private int subId;
+
     @Test
-    void testBoard(){
-        BufferedReader fr;
-        fr = new BufferedReader(new InputStreamReader(Objects.requireNonNull(this.getClass().getResourceAsStream("testdata/is_rug_valid_valid.txt"))));
-        Stream<String> testLines = fr.lines();
-        for (String line : testLines.toList()) {
-            String[] splitLine = line.split("@");
-            // For this test, there's two arguments needed to the function
-            Assertions.assertEquals(splitLine[2], String.valueOf(Marrakech.isRugValid(splitLine[0], splitLine[1])), splitLine[3]);
-
+    void testBoard() {
+        try {
+            BufferedReader fr = new BufferedReader(new InputStreamReader(Objects.requireNonNull(this.getClass().getResourceAsStream("testdata/is_rug_valid_valid.txt"))));
+            String line;
+            Pattern pattern = Pattern.compile("B(.{147})");  // Use capturing group to exclude 'B'
+            Random r = new Random();
+            int startPoint = r.nextInt(1000);
+            int count = startPoint;
+            while ((line = fr.readLine()) != null && count < startPoint + 50) {
+                    Matcher matcher = pattern.matcher(line);
+                    while (matcher.find()) {
+                        String extractedString = matcher.group(1);  // remove B
+                        
+                        Board board = new Board(extractedString);
+                        for (int i=0;i<49;i++) {
+                            char presentColor=extractedString.charAt(3*i);
+                            if(presentColor=='n'){
+                                assertNull(board.tiles[i].rug);
+                            }
+                            else {
+                                char subColor=extractedString.charAt(i*3);
+                                int subId=Integer.parseInt(extractedString.substring(i*3+1,i*3+3));
+                                assertEquals(presentColor,subColor);
+                                assertEquals(board.tiles[i].rug.id,subId);
+                            }
+                        }
+                }
+                count++;
             }
+            fr.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
+    }
 }
+
