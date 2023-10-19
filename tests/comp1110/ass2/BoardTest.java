1cb7c9ae5599c37fc2522c6d3e0fc03f2144ef1d
Administrator <u7759982@anu.edu.au>
2023-10-20 10:18:29 +1100

    :sparkles: perf: remove unused package imported, add assertion to check the transformation of positions

--- tests/comp1110/ass2/BoardTest.java
+++ tests/comp1110/ass2/BoardTest.java
@@ -1,48 +1,46 @@
 package comp1110.ass2;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.Objects;
 import java.util.Random;
-import java.util.stream.Stream;
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import static org.junit.jupiter.api.Assertions.*;
-class BoardTest {
-    private int subId;
 
+/**
+ * this test will read the file "testdata/is_rug_valid_valid.txt" given by lecture and random access 50 lines data
+ * first using board constructor construct data from this txt file into object
+ * using string operation to check whether the constructor generate the right information as the string
+ */
+class BoardTest {
     @Test
     void testBoard() {
         try {
             BufferedReader fr = new BufferedReader(new InputStreamReader(Objects.requireNonNull(this.getClass().getResourceAsStream("testdata/is_rug_valid_valid.txt"))));
             String line;
-            Pattern pattern = Pattern.compile("B(.{147})");  // Use capturing group to exclude 'B'
+            Pattern pattern = Pattern.compile("B(.{147})");  // use capturing group to exclude 'B'
             Random r = new Random();
-            int startPoint = r.nextInt(1000);
+            int startPoint = r.nextInt(1000);//random pick line start point from 0 to 1000
             int count = startPoint;
-            while ((line = fr.readLine()) != null && count < startPoint + 50) {
+            while ((line = fr.readLine()) != null && count < startPoint + 50) {//loop for 50 times
                     Matcher matcher = pattern.matcher(line);
                     while (matcher.find()) {
                         String extractedString = matcher.group(1);  // remove B
-                        
                         Board board = new Board(extractedString);
-                        for (int i=0;i<49;i++) {
+                        for (int i=0;i<49;i++) {//if null check the rug is null
                             char presentColor=extractedString.charAt(3*i);
                             if(presentColor=='n'){
                                 assertNull(board.tiles[i].rug);
                             }
-                            else {
+                            else {//not null check the color and id and position
                                 char subColor=extractedString.charAt(i*3);
                                 int subId=Integer.parseInt(extractedString.substring(i*3+1,i*3+3));
+                                IntPair subPosition=new IntPair(i/7,i%7);
                                 assertEquals(presentColor,subColor);
                                 assertEquals(board.tiles[i].rug.id,subId);
+                                assertEquals(board.tiles[i].position.x,subPosition.x);
+                                assertEquals(board.tiles[i].position.y,subPosition.y);
                             }
                         }
                 }
